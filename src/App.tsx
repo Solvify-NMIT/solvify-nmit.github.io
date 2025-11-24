05c920228902c281e8963b19fc1ed3dc7b7dfd36
Priyanshu8023 <priyanshukumar8023@gmail.com>
2025-11-24 23:29:08 +0530

    Revert "Merge pull request #4 from Solvify-NMIT/next"
    This reverts commit 430e2a9b587c21b5272972987830e3af93ec0ffd, reversing
    changes made to cf0dfa5c129b1b1d7de1dc5801077360aba5bd4f.

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import Navigation from './components/Navigation';
+import Header from './components/Header';
+import CodeAThon from './components/CodeAThon';
+import About from './components/About';
+import Events from './components/Events';
+import Projects from './components/Projects';
+import Team from './components/Team';
+import Founder from './components/Founder';
+import Timeline from './components/Timeline';
+import Gallery from './components/Gallery';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div className="bg-black min-h-screen">
+      <Navigation />
+      <Header />
+      <CodeAThon />
+      <About />
+      <Events />
+      <Projects />
+      <Team />
+      <Founder />
+      <Gallery />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
