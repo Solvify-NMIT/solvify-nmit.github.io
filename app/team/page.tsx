601089b0b85b512482f9b82d45e5a855f297a8bf
Priyanshu8023 <priyanshukumar8023@gmail.com>
2026-01-25 22:05:17 +0530

    fix: New Member Team Section

--- app/team/page.tsx
+++ app/team/page.tsx
@@ -67,7 +67,7 @@ const TeamPage = () => {
                                     </div>
 
                                     {/* Grid */}
-                                    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 md:gap-8">
+                                    <div className="grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 md:gap-8">
                                         {members.map((member, idx) => (
                                             <TeamCard
                                                 key={`${member.name}-${idx}`}
